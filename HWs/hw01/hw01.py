f39962b821823d23ac8d16340e9392a5e3b340ca
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-03-14 16:59:55 +0800

    feat: complete hw01

--- HWs/hw01/hw01.py
+++ HWs/hw01/hw01.py
@@ -14,9 +14,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 
@@ -44,7 +44,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return min(i, j, k) ** 2 + (i + j + k - min(i, j, k) - max(i, j, k)) ** 2
 
 
 def two_of_three_syntax_check():
@@ -69,6 +69,10 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(2, n):
+        if n % i == 0:
+            return n // i
+    return 1
 
 
 def hailstone(n):
@@ -91,3 +95,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    length = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = n * 3 + 1
+        length += 1
+        print(int(n))
+    return length
