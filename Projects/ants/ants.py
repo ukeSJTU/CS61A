1345e3334b003cec4d4090db0e81effd6c6a0704
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-05-09 12:51:14 +0800

    feat: complete problem 3 project ants

--- Projects/ants/ants.py
+++ Projects/ants/ants.py
@@ -174,7 +174,13 @@ class ThrowerAnt(Ant):
         This method returns None if there is no such Bee (or none in range).
         """
         # BEGIN Problem 3 and 4
-        return random_bee(self.place.bees)  # REPLACE THIS LINE
+        place = self.place
+        while len(place.bees) == 0 and not place.is_hive:
+            place = place.entrance
+        if place.is_hive:
+            return None
+        else:
+            return random_bee(place.bees)  # REPLACE THIS LINE
         # END Problem 3 and 4
 
     def throw_at(self, target):
